6e6b7f863d079cfde33e27d6b2b8af615cb3d07d
aakash jain <92187222+ajain202@users.noreply.github.com>
2022-03-15 21:12:33 -0500

    navigation set up & code base for category, rating and payment-detail components

--- farm-necessities-client/src/app.js
+++ farm-necessities-client/src/app.js
@@ -5,16 +5,24 @@ import styled from 'styled-components';
 import './app.css';
 import Cart from './components/cart/cart';
 import ProductCategory from './components/product-category/product-category';
+import Category from './components/category/category';
+import Rating from './components/rating/rating';
+import PaymentDetail from './components/payment-detail/payment-detail';
 import Product from './components/product/product';
 import Fonts from './styles/fonts';
+import Navigation from './components/navigation/navigation'
 
 const App = () => (
   <BrowserRouter>
     <Container fluid className="p-0">
+      <Navigation/>
       <Routes>
         <Route path="/cart" exact element={<Cart />} />
         <Route path="/product" exact element={<Product />} />
         <Route path="/product-category" exact element={<ProductCategory />} />
+        <Route path="/category" exact element={<Category />} />
+        <Route path="/rating" exact element={<Rating />} />
+        <Route path="/payment-detail" exact element={<PaymentDetail />} />
       </Routes>
     </Container>
   </BrowserRouter>
