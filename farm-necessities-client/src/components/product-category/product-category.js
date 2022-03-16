c6ea47f6eb979fdfdc0e0eb10c91869d79c8d6f8
Mayur Mule <34371256+mayur1711@users.noreply.github.com>
2022-03-15 20:45:10 -0500

    org structure setup & code base for cart, product, product-category components

---
+++ farm-necessities-client/src/components/product-category/product-category.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const ProductCategory = () => {
+  const columns = ['Product ID', 'Category ID'];
+  const operations = ['Create Product-Category', 'Read Product-Category', 'Update Product-Category', 'Delete Product-Category'];
+  return (
+    <Styles>
+      <Container fluid>
+        {operations.map((operation) => (
+          <div className="form-container">
+            <h3>{operation}</h3>
+            <Form>
+              {columns.map((field) => (
+                <Form.Group as={Row} className="mb-3">
+                  <Form.Label column sm={2}>
+                    {field}
+                  </Form.Label>
+                  <Col sm={10}>
+                    <Form.Control type="text" />
+                  </Col>
+                </Form.Group>
+              ))}
+              <Form.Group as={Row} className="mb-3">
+                <Col sm={{ span: 10, offset: 2 }}>
+                  <Button type="submit">{operation}</Button>
+                </Col>
+              </Form.Group>
+            </Form>
+          </div>
+        ))}
+      </Container>
+    </Styles>
+  );
+};
+
+const Styles = styled.div`
+  .container-fluid {
+    padding: 1rem;
+    margin-top: var(--nav-height);
+    max-width: 900px;
+  }
+
+  h3 {
+    margin-bottom: 2rem;
+    text-align: center;
+  }
+
+  .form-container {
+    width: 100%;
+    margin-top: 1rem;
+    margin-bottom: 3rem;
+  }
+`;
+
+export default ProductCategory;
