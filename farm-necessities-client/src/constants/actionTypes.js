15d98c99c8f05ccc33952376e25107ced86f8d42
Vedant <vmahes4@uic.edu>
2022-04-01 18:46:57 -0500

    pd4 & pd7 deliverable - full stack code base for get users task

--- farm-necessities-client/src/constants/actionTypes.js
+++ farm-necessities-client/src/constants/actionTypes.js
@@ -1,2 +1,4 @@
 export const AUTH = 'AUTH';
 export const LOGOUT = 'LOGOUT';
+export const FETCH_USERS = 'FETCH_USERS';
+export const CLEAR_USERS = 'CLEAR_USERS';
---
+++ farm-necessities-server/controllers/users.js
@@ -0,0 +1,13 @@
+import { databasePool, queryDatabase } from '../services/database.js';
+import { selectUser } from '../utilities/queries.js';
+
+export const getUsers = async (req, res) => {
+  try {
+    const placeholders = [];
+    const users = await queryDatabase(databasePool, selectUser.all, placeholders);
+    res.status(200).json(users);
+  } catch (error) {
+    console.log('get-users-error', error);
+    res.status(500).json({ message: 'Something went wrong' });
+  }
+};
