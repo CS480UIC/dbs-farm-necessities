291e18e111ba09db1c80a1732b737c09d668cba3
aakash jain <92187222+ajain202@users.noreply.github.com>
2022-04-27 14:17:52 -0500

    prodcut category abd query component backend codebase

--- farm-necessities-server/utilities/queries.js
+++ farm-necessities-server/utilities/queries.js
@@ -3,14 +3,16 @@ export const selectUser = {
   byEmail: 'SELECT * FROM user WHERE email = ?;',
   getIDs: 'SELECT DISTINCT user_id FROM user',
 };
-export const insertUser =
-  "INSERT INTO user (name, password, email, phone_number, account_type) VALUES (?, ?, ?, ?, ?);";
 
+
+export const insertUser =
+  'INSERT INTO user (name, password, email, phone_number, account_type) VALUES (?, ?, ?, ?, ?);';
 export const selectAddress = {
   all: 'SELECT * FROM address;',
   byID: 'SELECT * FROM address WHERE address_id = ?;',
   getIDs: 'SELECT address_id FROM address',
 };
+
 export const insertAddress = 'INSERT INTO address (user_id,address) VALUES(?, ?);';
 export const updateAddress = 'UPDATE address SET user_id= ?,address= ? WHERE address_id = ?;';
 export const deleteAddress = 'DELETE FROM address WHERE address_id = ?';
@@ -20,8 +22,6 @@ export const insertProduct = 'INSERT INTO product (user_id, name, description, p
 export const updateProduct = 'UPDATE product SET user_id= ?, name = ?, description = ?, price = ? WHERE product_id = ?;';
 export const deleteProduct = 'DELETE FROM product WHERE product_id = ?';
 
-export const view1 = "SELECT * FROM view_1;";
-
 export const selectCategory = {
   all: "SELECT * FROM category;",
 };
@@ -30,3 +30,20 @@ export const insertCategory =
 export const updateCategory =
   "UPDATE category SET name= ?,status= ? WHERE category_id = ?;";
 export const deleteCategory = "DELETE FROM category WHERE category_id = ?";
+export const queries = {
+  view1: 'SELECT * FROM view_1;',
+  view2: 'SELECT * FROM view_2;',
+  view3: 'SELECT * FROM view_3;',
+  view4: 'SELECT * FROM view_4;',
+  view5: 'SELECT * FROM view_5;',
+  view6: 'SELECT * FROM view_6;',
+  view7: 'SELECT * FROM view_7;',
+  view8: 'SELECT * FROM view_8;',
+  view9: 'SELECT * FROM view_9;',
+  view10: 'SELECT * FROM view_10;',
+};
+
+export const selectProductCategory = 'SELECT * FROM product_category;';
+export const insertProductCategory = 'INSERT INTO product_category (product_id,category_id) VALUES(?, ?);';
+export const updateProductCategory = 'UPDATE product_category SET product_id= ?, category_id = ? WHERE product_category_id = ?;';
+export const deleteProductCategory = 'DELETE FROM product_category WHERE product_category_id = ?;';
