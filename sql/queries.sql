1e251885b72b277a6ef02443e67e03882d47d8f1
aakash jain <92187222+ajain202@users.noreply.github.com>
2022-04-26 22:35:05 -0500

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -4,7 +4,7 @@ CREATE VIEW view_3 as SELECT * FROM cart where user_id in (3,2) order by quantit
 CREATE VIEW view_4 as SELECT concat(name,' ',description) FROM product;
 CREATE VIEW view_5 as SELECT user_id,count(*) as number_of_orders FROM order_history group by user_id;
 CREATE VIEW view_6 as SELECT order_id,day(delivery_date) FROM order_history where month(delivery_date)>5;
-CREATE VIEW view_7 as select user_id,card_num from payment_detail where payment_id in (1,3,5) order by card_num desc;
+CREATE VIEW view_7 as select user_id,card_number from payment_detail where payment_id in (1,3,5) order by card_number desc;
 CREATE VIEW view_8 as SELECT b.name as product_name,c.name as category_name FROM product_category a 
 inner join product b on a.product_id=b.product_id 
 inner join category c on a.category_id=c.category_id;
