fa5ce177aac252885be08387c04f8b5322c7e277
mmule2 <mmule2@uic.edu>
2022-04-27 02:25:07 -0500

    added frontend for address component

--- farm-necessities-client/src/api/index.js
+++ farm-necessities-client/src/api/index.js
@@ -13,3 +13,7 @@ export const login = (formData) => API.post('/auth/login', formData);
 export const signup = (formData) => API.post('/auth/signup', formData);
 export const fetchUsers = () => API.get('/user');
 export const initializeDatabase = () => API.get('/initialize-database');
+export const createAddress = (addressData) => API.post('/address', addressData);
+export const retrieveAddresses = () => API.get('/address');
+export const updateAddress = (addressData) => API.put('/address', addressData);
+export const deleteAddress = (addressData) => API.delete('/address', { data: addressData });
