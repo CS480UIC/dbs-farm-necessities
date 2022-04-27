251113453cfd1ddef5f7b36362a2d12e90809def
mmule2 <mmule2@uic.edu>
2022-04-27 04:47:53 -0500

    revert redux implementation for address component

--- farm-necessities-client/src/constants/actionTypes.js
+++ farm-necessities-client/src/constants/actionTypes.js
@@ -2,7 +2,3 @@ export const AUTH = 'AUTH';
 export const LOGOUT = 'LOGOUT';
 export const FETCH_USERS = 'FETCH_USERS';
 export const CLEAR_USERS = 'CLEAR_USERS';
-export const CREATE_ADDRESS = 'CREATE_ADDRESS';
-export const RETRIEVE_ADDRESSES = 'RETRIEVE_ADDRESSES';
-export const UPDATE_ADDRESS = 'UPDATE_ADDRESS';
-export const DELETE_ADDRESS = 'DELETE_ADDRESS';
--- farm-necessities-client/src/reducers/index.js
+++ farm-necessities-client/src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import auth from './authReducer';
 import users from './usersReducer';
-import addresses from './addressReducer';
 
 export default combineReducers({
   auth,
   users,
-  addresses,
 });
